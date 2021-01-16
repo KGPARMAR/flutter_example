b909b7bb710d633e8418d87f9fa95f845eca23cb
Kishor Parmar <kishorp@hyperlinkinfosystem.net.in>
2021-01-16 17:14:00 +0530

    [+] print.dart [+] comment.dart [-] remove widget_test.dart file

---
+++ lib/comment.dart
@@ -0,0 +1,25 @@
+/*
+*    @author     KGPARMAR
+*    @project    flutter_example
+*    @file_name  comment
+*    @desc       Flutter Developer
+*    @version    1.0.0
+*    @since      1.0
+*    @created on 16-Jan-21 5:07 PM
+*    @updated on 16-Jan-21 5:07 PM
+*    @Notes       
+*/
+
+void main(){
+  // double forward splash to write comment and comment code are not run
+
+  // this is comment
+
+  //i hope you are understanding how to add comment
+
+  /*
+  *
+  * This is multi line comment
+  *
+  * */
+}
---
+++ lib/print.dart
@@ -0,0 +1,37 @@
+/*
+ *    @author     KGPARMAR
+ *    @project    flutter_example
+ *    @file_name  main
+ *    @desc       Flutter Developer
+ *    @version    1.0.0
+ *    @since      1.0
+ *    @created on 16-Jan-21 4:58 PM
+ *    @updated on 16-Jan-21 4:58 PM
+ *    @Notes
+ */
+
+void main() {
+  //print data on console using "print"
+  //end of print need to semicolon to end
+
+  // print string data
+  print("Hello dart!!");
+  // print string data using single quites
+  print('This is my first application');
+
+  //print integer data
+  print(47);
+
+  //print boolean data
+  print(true);
+
+  //magmatic operation
+  print(4 / 3);
+
+  //print string with sum dynamic value
+  String name = "KGPARMAR";
+  print("M y name is : $name");
+
+  //add arithmetic operation in print
+  print("Two number sum : ${40 + 7}");
+}

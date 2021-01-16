2590a7ce20f15707251efca0ffda8cdfdcd6587e
KGPARMAR <parmar.kishor76@gmail.com>
2021-01-16 17:32:40 +0530

    [+] data type and variables

---
+++ lib/data_type_and_variables.dart
@@ -0,0 +1,34 @@
+/*
+*    @author     KGPARMAR
+*    @project    flutter_example
+*    @file_name  data_type_and_variables
+*    @desc       Flutter Developer
+*    @version    1.0.0
+*    @since      1.0
+*    @created on 16-Jan-21 5:27 PM
+*    @updated on 16-Jan-21 5:27 PM
+*    @Notes      data type and variables
+*/
+
+void main() {
+  //default value are null
+
+  // this is variable you show you this is not convert based on value
+  String name = "KGPARMAR";
+  String code;
+  bool isValid = true;
+
+  // this is variable below you show using var keyword  var is store and convert base on value
+  var job = "developer";
+  var company = false;
+  var isFreelancer = true;
+  var number = 47;
+
+  print('name : $name');
+  print('code : $code');
+  print('isValid : $isValid');
+  print('job : $job');
+  print('company : $company');
+  print('isFreelancer : $isFreelancer');
+  print('number : $number');
+}
